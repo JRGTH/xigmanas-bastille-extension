b3578d7c9a09a9c58d08945bb1b6e731a1f9c226
Jose <joserprg@gmail.com>
2021-01-04 16:37:16 -0400

    Minor code changes

--- gui/bastille_manager_maintenance.php
+++ gui/bastille_manager_maintenance.php
@@ -341,7 +341,7 @@ $(document).ready(function(){
 				</tr>
 					<?php
 						if(!initial_install_banner()):
-							html_checkbox2('zfs_activate',gtext('ZFS support activation'),'' ? true : false,gtext('Check this to activate ZFS support or leave unchecked to dismiss, this is a one time option and this row will disappear after clicking Save button.'),'',false);
+							html_checkbox2('zfs_activate',gtext('ZFS support activation'),'' ? true : false,gtext('Check this to activate ZFS support or leave unchecked to dismiss (requires ZFS support to be available), this is a one time option and this row will disappear after clicking Save button.'),'',false);
 						endif;
 					?>
 					<?php html_filechooser("backup_path", gtext("Backup directory"), $backup_path, gtext("Directory to store containers backup archives, use as file chooser for restoring from file."), $backup_path, true, 60);?>
