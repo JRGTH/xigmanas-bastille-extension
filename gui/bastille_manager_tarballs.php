df423e75052cbc06f8f3c2c2ac3cd2e3680f0af7
JRGTH <joserprg@gmail.com>
2023-05-01 14:26:51 -0400

    Update release list to include 13.2

--- gui/bastille_manager_tarballs.php
+++ gui/bastille_manager_tarballs.php
@@ -72,6 +72,7 @@ $sphere_array = $rel_list;
 
 if ($linux_compat_support == "YES"):
 	$a_action = [
+		'13.2-RELEASE' => gettext('13.2-RELEASE'),
 		'13.1-RELEASE' => gettext('13.1-RELEASE'),
 		'13.0-RELEASE' => gettext('13.0-RELEASE'),
 		'12.3-RELEASE' => gettext('12.3-RELEASE'),
@@ -90,6 +91,7 @@ if ($linux_compat_support == "YES"):
 	];
 else:
 	$a_action = [
+		'13.2-RELEASE' => gettext('13.2-RELEASE'),
 		'13.1-RELEASE' => gettext('13.1-RELEASE'),
 		'13.0-RELEASE' => gettext('13.0-RELEASE'),
 		'12.3-RELEASE' => gettext('12.3-RELEASE'),
